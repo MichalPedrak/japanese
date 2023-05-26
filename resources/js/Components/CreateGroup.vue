ad800a53a6f72d21797e83296a426b0c99e31f05
MichalPedrak <michalpedrak07@gmail.com>
2023-05-26 23:47:39 +0200

    Add buttons to add group and cards

--- resources/js/Components/CreateGroup.vue
+++ resources/js/Components/CreateGroup.vue
@@ -2,21 +2,23 @@
             <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Dodaj nową grupę</h2>
             <div class="mb-6 flex gap-5">
 
-                <input v-model="groupData.title"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nazwa nowej grupy" required>
-                <button @click.prevent="addGroup" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dodaj</button>
+<!--                <input v-model="groupData.title"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nazwa nowej grupy" required>-->
+                <button @click.prevent="store.changeStep(4)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dodaj grupę</button>
+                <button @click.prevent="store.changeStep(5)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dodaj fiszkę</button>
+
             </div>
 </template>
 
 <script setup>
 
 import { useForm } from "@inertiajs/vue3";
-
-
+import {useCardStore} from "@/store/CardsStore";
 
 
 let groupData = useForm({
     title: "",
 })
+let store = useCardStore();
 
 const addGroup = async () => {
 
--- resources/js/Pages/CardsView.vue
+++ resources/js/Pages/CardsView.vue
@@ -91,6 +91,66 @@
 
 
     </div>
+    <div v-if="store.showStep == 4" class="relative mt-5 content w-75 text-center" style="background: lightblue; height: 250px; border-radius: 8px;">
+
+        <button class="z-10 absolute top-0 left-0 w-10 border h-10"  @click="store.changeStep(1)">&lt;</button>
+
+        <section class="border-gray-200 border relative bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto nphplg:py-16">
+
+                <h2>Fiszkii</h2>
+                <CreateGroup class="sa"></CreateGroup>
+
+                <div class="w-full  p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex items-center justify-between mb-4">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Fiszki</h5>
+                        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                            <!--                    View all-->
+                        </a>
+                    </div>
+                    <div  class="flow-root">
+                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                            DODAJ Grupę
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+
+
+    </div>
+    <div v-if="store.showStep == 5" class="relative mt-5 content w-75 text-center" style="background: lightblue; height: 250px; border-radius: 8px;">
+
+        <button class="z-10 absolute top-0 left-0 w-10 border h-10"  @click="store.changeStep(1)">&lt;</button>
+
+        <section class="border-gray-200 border relative bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto nphplg:py-16">
+
+                <h2>Fiszkii</h2>
+                <CreateGroup class="sa"></CreateGroup>
+
+                <div class="w-full  p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex items-center justify-between mb-4">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Fiszki</h5>
+                        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                            <!--                    View all-->
+                        </a>
+                    </div>
+                    <div  class="flow-root">
+                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                            DODAJ fiszkę
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+
+
+    </div>
 </template>
 
 <script>
